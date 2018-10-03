mkk255/Angular2Excercises

step 10: make API_ENDPOINT injectable

src/app/contacts.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { Http } from '@angular/http';
 
 import 'rxjs/add/operator/map';
@@ -6,17 +6,15 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ContactsService {
 
-  private API_ENDPOINT = 'http://localhost:4201/api';
-
-  constructor(private http: Http) {}
+  constructor(private http: Http, @Inject('API_ENDPOINT') private apiEndpoint) {}
 
   getContact (id: string) {
-    return this.http.get(`${this.API_ENDPOINT}/contacts/${id}`)
+    return this.http.get(`${this.apiEndpoint}/contacts/${id}`)
                     .map(res => res.json().item);
   }
 
   getContacts () {
-    return this.http.get(`${this.API_ENDPOINT}/contacts`)
+    return this.http.get(`${this.apiEndpoint}/contacts`)
                     .map(res => res.json())
                     .map(data => data.items);
   }

seanforyou23/angular-master-class-jump-start

src/app/contacts.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
@@ -13,18 +13,16 @@ interface ContactsResponse { items : Contact[]  }
 @Injectable()
 export class ContactsService {
 
-  private API_ENDPOINT = 'http://localhost:4201/api';
-
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, @Inject('API_ENDPOINT') private apiEndpoint) {}
 
 
   getContact(id: string): Observable<Contact> {
-    return this.http.get<ContactResponse>(`${this.API_ENDPOINT}/contacts/${id}`)
+    return this.http.get<ContactResponse>(`${this.apiEndpoint}/contacts/${id}`)
         .pipe(map(data => data.item));
   }
 
   getContacts(): Observable<Array<Contact>> {
-    return this.http.get<ContactsResponse>(`${this.API_ENDPOINT}/contacts`)
+    return this.http.get<ContactsResponse>(`${this.apiEndpoint}/contacts`)
         .pipe(map(data => data.items));
   }
 